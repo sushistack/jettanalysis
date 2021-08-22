149a410b4879ca6ec66c9d4934a7fb1eaf281de4
deepIify <deepIify@users.noreply.github.com>
2021-08-22 07:14:26 +0000

    deploy: 432f8a8fc863a9b032de3ae6b816a824e45df619

--- _nuxt/ecf51be.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{325:function(o,n,t){"use strict";t.r(n);n.default=[{name:"홈",to:"/"},{name:"사이트 진단",to:"/diagnosis"},{name:"블로그",to:"/blog"},{name:"소개",to:"/about"}]}}]);
--- _nuxt/static/1629601800/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fblog","\u002Fcontact","\u002Fprivacy-notice","\u002Fdiagnosis","\u002Fterms-of-service","\u002Fblog\u002Ftest-bloging"]})
