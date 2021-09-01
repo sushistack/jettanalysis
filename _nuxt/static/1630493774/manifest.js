4cc1f19730059ab38edd21195bbdd9b349cc48d5
sohnsmith <sohnsmith@users.noreply.github.com>
2021-09-01 12:18:46 +0000

    deploy: 037904ca243dff52de98c690f81aef2268099a14

--- _nuxt/static/1630493774/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fblog","\u002Fcontact","\u002Fdiagnosis","\u002Fprivacy-notice","\u002Fterms-of-service","\u002Fwiki","\u002Fabout","\u002Fblog\u002Fhow-to-write-nice-title","\u002Fwiki\u002Fwiki-test"]})
---
+++ _nuxt/static/1630498721/diagnosis/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630498721",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdiagnosis",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1ff999a6"}}}}(null));
