72c50a9c868212fac86caa7c9aa821eff2344067
sohnsmith <sohnsmith@users.noreply.github.com>
2021-09-01 10:27:59 +0000

    deploy: 6af7f835442d0d15cee20452607bfce77833dcdc

--- _nuxt/static/1630469882/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630469882",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"21234d1a"}}}}(null,"\u002F"));
