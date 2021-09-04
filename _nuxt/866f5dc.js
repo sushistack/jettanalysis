605853680d0d12563d34410db7ad7d9b34df2aa3
deepIify <deepIify@users.noreply.github.com>
2021-09-04 14:38:39 +0000

    deploy: 19d72b847a89f7ab0c3167fad0dc8b1c2492c51e

--- _nuxt/866f5dc.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{370:function(o,n,t){"use strict";t.r(n);n.default=[{name:"홈",to:"/"},{name:"사이트 진단",to:"/diagnosis"},{name:"블로그",to:"/blog"},{name:"위키",to:"/wiki"},{name:"소개",to:"/about"}]}}]);
---
+++ _nuxt/static/1630766315/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630766315",layout:"default",error:a,serverRendered:true,routePath:b,config:{googleAnalytics:{id:"UA-206750180-1"},_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"40847fd2"}}}}(null,"\u002F"));
