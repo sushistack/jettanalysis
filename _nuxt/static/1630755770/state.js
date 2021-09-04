3e86a611fd9ee50cf2a6a17faa1e5bd723ef6301
deepIify <deepIify@users.noreply.github.com>
2021-09-04 14:00:12 +0000

    deploy: 06f818e354ccdc39085efcdffd968d04cb8fe5d8

--- _nuxt/static/1630755770/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630755770",layout:"default",error:a,serverRendered:true,routePath:b,config:{googleAnalytics:{id:"UA-206750180-1"},_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f954b99c"}}}}(null,"\u002F"));
