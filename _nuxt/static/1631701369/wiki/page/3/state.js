8b468d53130520aa00091c3f771b25dfaf3abf82
deepIify <deepIify@users.noreply.github.com>
2021-09-15 10:22:55 +0000

    deploy: 5b50e2a71b721ddae7375cb29b4f6896c965537f

---
+++ _nuxt/static/1631701369/wiki/page/3/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631701369",layout:"error",error:{statusCode:404,message:"포스트가 없습니다."},serverRendered:false,routePath:"\u002Fwiki\u002Fpage\u002F3",config:{googleAnalytics:{id:"UA-206750180-1"},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"c3553975"}}};
