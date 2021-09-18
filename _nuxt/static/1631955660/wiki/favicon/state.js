ee987f61afa092c49361f5380c0225465e62d8fa
deepIify <deepIify@users.noreply.github.com>
2021-09-18 13:52:12 +0000

    deploy: 928563bdc066d6a9047cdbe8f3402a89a11af4c7

--- _nuxt/static/1631955660/wiki/favicon/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631955660",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwiki\u002Ffavicon",config:{googleAnalytics:{id:"UA-206750180-1"},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f1b64c43"}}}}(null));
