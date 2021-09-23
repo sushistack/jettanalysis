2b964308cad78924abb424382ec014bee58d16ea
deepIify <deepIify@users.noreply.github.com>
2021-09-23 09:17:20 +0000

    deploy: b5f0adfef0347bf95a225f13ff7b72943aaaa18f

---
+++ _nuxt/static/1632388626/wiki/mobile-optimization/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632388626",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwiki\u002Fmobile-optimization",config:{googleAnalytics:{id:"UA-206750180-1"},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ee0888d6"}}}}(null));
