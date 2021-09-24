5b9ec51d5628428e988eaebfd0ad7e643571c9bc
deepIify <deepIify@users.noreply.github.com>
2021-09-24 07:10:24 +0000

    deploy: ddcb3b562dc77fca94bc35eb119d477a393bf7c4

---
+++ _nuxt/static/1632467410/wiki/page/4/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632467410",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwiki\u002Fpage\u002F4",config:{googleAnalytics:{id:"UA-206750180-1"},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ea79940d"}}}}(null));
