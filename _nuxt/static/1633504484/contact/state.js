4a8db194720d9866c6cc85dd182b8d682b0ad0f6
deepIify <deepIify@users.noreply.github.com>
2021-10-06 08:33:16 +0000

    deploy: 810ee7fefc2a6119df60484ba5831823625f4db6

--- _nuxt/static/1633504484/contact/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633504484",layout:"default",error:a,state:{auth:{token:a},user:{user:a,isAdmin:false}},serverRendered:true,routePath:"\u002Fcontact",config:{googleAnalytics:{id:"UA-206750180-1"},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7f5f8cc0"}}}}(null));
