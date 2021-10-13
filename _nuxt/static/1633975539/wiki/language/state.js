692c83c0c3d2768c35e98a0a32d35b5db0169f7c
deepIify <deepIify@users.noreply.github.com>
2021-10-13 15:08:37 +0000

    deploy: b4a00477ac7cca5c2b2b5c85fb9b98419b2750c6

--- _nuxt/static/1633975539/wiki/language/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633975539",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwiki\u002Flanguage",config:{googleAnalytics:{id:"UA-206750180-1"},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8a346ab7"}}}}(null));
