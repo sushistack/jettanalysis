061b3fabaadb9fdac9adec07012c2f99d78165f4
deepIify <deepIify@users.noreply.github.com>
2021-10-14 09:49:26 +0000

    deploy: be27852d8cd3104b25826f987bc1278f413f3776

--- _nuxt/static/1634187039/wiki/session-id/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634187039",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwiki\u002Fsession-id",config:{googleAnalytics:{id:"UA-206750180-1"},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6133d91c"}}}}(null));
---
+++ _nuxt/static/1634204953/blog/seven-things-of-websites-google-loves/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634204953",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fseven-things-of-websites-google-loves",config:{googleAnalytics:{id:"UA-206750180-1"},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7b5d9bbb"}}}}(null));
