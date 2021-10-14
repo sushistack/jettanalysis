3ff0131686d6e452645165b571c6d5a193a469a1
deepIify <kjm71712@naver.com>
2021-10-14 18:48:45 +0900

    update

--- components/data/menu.js
+++ components/data/menu.js
@@ -1,8 +1,8 @@
 const MENU = [
   { name: '사이트 진단', to: '/diagnosis' },
   { name: '위키', to: '/wiki' },
-  { name: '링크스왑', to: '#linkswap' },
-  { name: '블로그', to: '/blog' }
+  { name: '블로그', to: '/blog' },
+  { name: '링크스왑', to: '#linkswap' }
 ]
 
 export default MENU
