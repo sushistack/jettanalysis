8ae490e58e976b7c47d637308ecf11cb78f85a97
deepIify <deepIify@users.noreply.github.com>
2021-10-15 02:22:55 +0000

    deploy: 41e2bdf77606be9466c7d78acec51e4229e4eddc

--- _nuxt/static/1634264443/blog/page/2/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634264443",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fpage\u002F2",config:{googleAnalytics:{id:"UA-206750180-1"},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8ea20d74"}}}}(null));
---
+++ _nuxt/static/1634264560/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634264560",layout:"default",error:a,serverRendered:true,routePath:b,config:{googleAnalytics:{id:"UA-206750180-1"},_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e701b099"}}}}(null,"\u002F"));
